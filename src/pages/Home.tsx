269494867792f0d5f2475759e32d8e42b6e08a51
thieryw <williamalexthiery@hotmail.fr>
2022-03-18 00:24:14 +0100

    start Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,37 @@
-import {memo} from "react";
+import { memo } from "react";
+import { ImageSliderBackground } from "../components/ImageSliderBackground";
+import { makeStyles } from "../theme";
+import slide1 from "../assets/img/home/Slide1.jpg";
+import slide2 from "../assets/img/home/Slide2.jpg";
+import slide3 from "../assets/img/home/Slide3.jpg";
+import { useTheme } from "gitlanding/theme";
 
-export const Home = memo(()=>{
-	return <footer>
+export const Home = memo(() => {
+	const { paddingRightLeft } = useTheme();
+	const { classes } = useStyles({paddingRightLeft});
+	return <section>
+		<div className={classes.sliderBackground}>
+			<ImageSliderBackground
+				imageUrls={[
+					slide1,
+					slide2,
+					slide3,
+				]}
+			/>
 
-	</footer>
+		</div>
+
+	</section>
 })
+
+const useStyles = makeStyles<{paddingRightLeft: number}>()(
+	(...[,{paddingRightLeft}]) => ({
+		"sliderBackground": {
+			"position": "relative",
+			"left": -paddingRightLeft,
+			"width": window.innerWidth,
+			"height": window.innerHeight,
+		}
+
+	})
+)
